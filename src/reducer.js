0f72b0b4ced884922a6568a1c0eb1fc5ea91e196
mckmillions <zachmckimmins@gmail.com>
2016-12-08 17:26:23 -0600

    pre lecture

--- src/reducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-
-import user from "./ducks/userDuck";
-import account from "./ducks/accountDuck";
-
-export default combineReducers( {
-	  account
-	, user
-} );
